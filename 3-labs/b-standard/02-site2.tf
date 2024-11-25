85ec4030d5e66e7680088571fab0f578781dbca1
Kayode Salawu <salawu@gmail.com>
2024-11-25 08:21:18 +0000

    update

--- 3-labs/b-standard/02-site2.tf
+++ 3-labs/b-standard/02-site2.tf
@@ -92,7 +92,7 @@ module "site2_vpc_firewall" {
       destination_ranges = ["::/0", ]
       rules              = [{ protocol = "tcp", ports = [25, ] }]
     }
-    "${local.site2_prefix}allow-egress-all" = {
+    "${local.site2_prefix}allow-egress-all-ipv6" = {
       priority           = 1001
       deny               = false
       description        = "allow egress"
