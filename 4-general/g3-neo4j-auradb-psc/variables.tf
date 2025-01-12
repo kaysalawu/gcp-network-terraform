f53303078bd5c737ab04a24a910e852586cf5833
Kayode Salawu <salawu@gmail.com>
2025-01-12 11:27:23 +0000

    variables update

--- 4-general/g3-neo4j-auradb-psc/variables.tf
+++ 4-general/g3-neo4j-auradb-psc/variables.tf
@@ -17,20 +17,3 @@ variable "folder_id" {
 variable "project_id_hub" {
   description = "hub project id"
 }
-
-variable "project_id_host" {
-  description = "host project id"
-}
-
-variable "project_id_spoke1" {
-  description = "spoke1 project id (service project id attached to the host project"
-}
-
-variable "project_id_spoke2" {
-  description = "spoke2 project id (standalone project)"
-}
-
-variable "project_id_onprem" {
-  description = "onprem project id (for onprem site1 and site2)"
-}
-
